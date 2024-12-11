2d4ae3e1192c751617a7bf4d82d2f85f374f2ca1
SebMZI <mrgy.sebastien@gmail.com>
2024-12-11 12:01:40 +0100

    feat: add a new way of importing tebexjs

--- app/basket/page.jsx
+++ app/basket/page.jsx
@@ -3,8 +3,10 @@ import React from "react";
 import Topbar from "../components/Topbar";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Basket from "../components/Basket";
+import dynamic from "next/dynamic";
+// import Basket from "../components/Basket";
 
+const Basket = dynamic(() => import("../components/Basket"), { ssr: false });
 const page = () => {
   return (
     <>
