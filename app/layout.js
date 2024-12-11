432c4d44f42cd2104025b41e04b7706a0d22d615
SebMZI <mrgy.sebastien@gmail.com>
2024-12-11 15:07:42 +0100

    feat: add Microsoft clarity to project

--- app/layout.js
+++ app/layout.js
@@ -24,6 +24,19 @@ export default function RootLayout({ children }) {
         >
           {children}
         </basketContext.Provider>
+
+        {/* Clarity Analytics */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `          
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "pcc5d3kts5");`,
+          }}
+        ></script>
       </body>
     </html>
   );
