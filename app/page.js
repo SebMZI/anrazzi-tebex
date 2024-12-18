46048c525495e63781bc4888c33c591e49f9edb1
SebMZI <mrgy.sebastien@gmail.com>
2024-12-18 11:04:58 +0100

    fix: missing quote

--- app/page.js
+++ app/page.js
@@ -4,15 +4,15 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 export const metadata = {
-  title:
-    "Anrazzi - Premium FiveM Scripts & Tools for GTA 5 Servers",
+  title: "Anrazzi - Premium FiveM Scripts & Tools for GTA 5 Servers",
   description:
-    "Boost your GTA 5 roleplay server with premium FiveM scripts and custom resources. Explore exclusive tools designed to enhance your gaming experience today!"keywords: ["FiveM", "GTA5", "Scrips", "Fivem Server", "Resources"],
+    "Boost your GTA 5 roleplay server with premium FiveM scripts and custom resources. Explore exclusive tools designed to enhance your gaming experience today!",
+  keywords: ["FiveM", "GTA5", "Scrips", "Fivem Server", "Resources"],
   openGraph: {
-    title:
-      "Anrazzi - Premium FiveM Scripts & Tools for GTA 5 Servers",
+    title: "Anrazzi - Premium FiveM Scripts & Tools for GTA 5 Servers",
     description:
-      "Boost your GTA 5 roleplay server with premium FiveM scripts and custom resources. Explore exclusive tools designed to enhance your gaming experience today!"  url: "https://anrazzi.fr",
+      "Boost your GTA 5 roleplay server with premium FiveM scripts and custom resources. Explore exclusive tools designed to enhance your gaming experience today!",
+    url: "https://anrazzi.fr",
     siteName: "Anrazzi",
     images: [
       {
@@ -26,10 +26,10 @@ export const metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title:
-      "Anrazzi - Premium FiveM Scripts & Tools for GTA 5 Servers",
+    title: "Anrazzi - Premium FiveM Scripts & Tools for GTA 5 Servers",
     description:
-      "Boost your GTA 5 roleplay server with premium FiveM scripts and custom resources. Explore exclusive tools designed to enhance your gaming experience today!"  images: {
+      "Boost your GTA 5 roleplay server with premium FiveM scripts and custom resources. Explore exclusive tools designed to enhance your gaming experience today!",
+    images: {
       url: "/images/anrazzi-opengraph.webp",
       alt: "Anrazzi Logo",
     },
