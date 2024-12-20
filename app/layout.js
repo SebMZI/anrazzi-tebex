6ab879c6004f1c9d43f2338c5c24aa1ddce132f5
SebMZI <mrgy.sebastien@gmail.com>
2024-12-20 16:13:27 +0100

    feat: add cookies consent

--- app/layout.js
+++ app/layout.js
@@ -31,14 +31,49 @@ export default function RootLayout({ children }) {
         <script
           type="text/javascript"
           dangerouslySetInnerHTML={{
-            __html: `          
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "pcc5d3kts5");`,
+            __html: `                       
+              (function(c,l,a,r,i,t,y){                 
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};                 
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;                 
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);             
+              })(window, document, "clarity", "script", "pcc5d3kts5");
+            `,
           }}
         ></script>
+
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `                       
+              window.axeptioSettings = {               
+                clientId: "676587bd75e639c447bd7f85",               
+                cookiesVersion: "anrazzi-fr-EU",             
+              };                          
+              (function(d, s) {               
+                var t = d.getElementsByTagName(s)[0], e = d.createElement(s);               
+                e.async = true; e.src = "//static.axept.io/sdk.js";               
+                t.parentNode.insertBefore(e, t);             
+              })(document, "script");             
+            `,
+          }}
+        ></script>
+
+        {/* New axeptio callback function for cookie completion */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `               
+              void 0 === window._axcb && (window._axcb = []);             
+              window._axcb.push(function(axeptio) {               
+                axeptio.on("cookies:complete", function(choices) {                 
+                                  
+                  console.log(choices);               
+                })             
+              });             
+            `,
+          }}
+        ></script>
+
         {/* Vercel Analytics */}
         <Analytics />
         {/* Vercel Speed Insights */}
