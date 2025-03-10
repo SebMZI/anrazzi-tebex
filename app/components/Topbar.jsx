5b561081ff4a9b122d02ccf1eb8eb9f6f43f6a4e
SebMZI <49833807+SebMZI@users.noreply.github.com>
2025-03-10 19:42:44 +0100

    update topbar

--- app/components/Topbar.jsx
+++ app/components/Topbar.jsx
@@ -2,8 +2,8 @@ const Topbar = () => {
   return (
     <div className="bg-ascent text-sm py-2 text-center">
       <p className="text-black">
-        <span className=" font-bold">10% off</span> your next purchase with code{" "}
-        <span className="font-bold">JOB10</span>
+        <span className=" font-bold">NEW</span> PREMIUM PACK{" "}
+        <span className="font-bold">Get all the scripts in one</span>
       </p>
     </div>
   );
